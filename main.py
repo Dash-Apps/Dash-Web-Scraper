10880e3cd0b4c4f152aa9cbcdd426b66acabd53b
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-03-20 15:37:44 +0000

    gui file

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-import flask
-import logging
-from flask import Flask, request
-import requests
-list = open("urls.txt","a")
-URL = "https://coolmathgames.com"
-
-app = Flask(__name__)
-@app.route('/')
-def main():
-    new_url = f"{URL}{request.full_path}"
-    return requests.get(url=new_url).text
-
-@app.route('/<path:path>')
-def catchall(path):
-    list.write(path)
-    # -- Drop Certain Requests --
- 
-    if "ads" in path:
-        return "No ads!"
-    #  --- Send out request and return it ---
-    new_url = f"{URL}{request.full_path}"
-    return requests.get(url=new_url).text
-
-app.run(debug=True,port=8080)
+from pywebcopy import save_webpage
+url = 'https://www.coolmathgames.com/'
+folder = 'Desktop/March 2023'
+kwargs = {'bypass_robots': True, 'project_name': 'sample_webpage'}
+save_webpage(url, folder, **kwargs)
+print("webpage saved in the location:",folder)
