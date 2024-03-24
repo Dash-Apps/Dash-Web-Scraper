0017d9c98513d385ea7b08d13478610a81196041
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-03-24 04:18:31 +0000

    started working on func to get the general data from a website such as cookies and response time

---
+++ wz_apps/webping.py
@@ -0,0 +1,5 @@
+from werkzeug.wrappers import Request
+
+def application(environ,start_response):
+    start_response('200 OK',[('Content-Type','text/plain')])
+    return ['Hello World!'.encode('utf-8')]
